d7879ea6a46cda74701988f506c40ad88fd5c3e2
James Kyan <james.kyan88@gmail.com>
2022-07-01 06:13:59 -0400

    commit tree 68da1e4bd5cb87e00f1e5b809855879657ee6ed7

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+  attr_accessor :length_1, :length_2, :length_3
+  
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+  def kind
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end
+
+  class TriangleError < StandardError
+    def message
+      "you must give the kind method and arguments of an instance of the triangle class!"
+    end
+  end
 end
